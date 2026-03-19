321aceb324e1366f58d70e16ca4738707635a3ae
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-03-20 00:07:02 +0100

    feat: add app-btn to the footer

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { Button } from "../button/button";
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-footer",
-  imports: [RouterLink],
+  imports: [RouterLink, Button],
   templateUrl: "./footer.html",
   styleUrl: "./footer.scss",
 })
