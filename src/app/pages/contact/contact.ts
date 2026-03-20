516bc8a6d29094786667677c3c0f67b598d62085
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-03-20 22:42:50 +0100

    feat: add contact page

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
+import { Button } from "../../shared/button/button";
 
 @Component({
   selector: "app-contact",
-  imports: [],
+  imports: [Button],
   templateUrl: "./contact.html",
   styleUrl: "./contact.scss",
 })
