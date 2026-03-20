7d4097e9b1bd5f3660e657abc97f190852745345
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2026-03-20 19:20:44 +0100

    feat: add care for section

---
+++ src/app/pages/landing/components/care-for/care-for.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { Button } from "../../../../shared/button/button";
+
+@Component({
+  selector: "app-care-for",
+  imports: [Button],
+  templateUrl: "./care-for.html",
+  styleUrl: "./care-for.scss",
+})
+export class CareFor {}
--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -4,11 +4,12 @@ import { Welcome } from "./components/welcome/welcome";
 import { Meet } from "./components/meet/meet";
 import { PayFeed } from "./components/pay-feed/pay-feed";
 import { OurUser } from "./components/our-user/our-user";
+import { CareFor } from "./components/care-for/care-for";
 
 
 @Component({
   selector: "app-landing",
-  imports: [Favorite, Welcome, Meet, PayFeed, OurUser],
+  imports: [Favorite, Welcome, Meet, PayFeed, OurUser, CareFor],
   templateUrl: "./landing.html",
   styleUrl: "./landing.scss",
 })
